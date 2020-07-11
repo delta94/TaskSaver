d7e035ea715542539062d36268964124c9a6bdee
guybdd <guybdd123@gmail.com>
2020-07-11 19:22:29 +0300

    deploy

--- server/api/routes/tasks.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllTasks, createTask, updateTask, deleteTask } from '../controllers/tasks';
-export const tasksRoutes = express.Router();
-
-tasksRoutes.get('/:id', getAllTasks);
-tasksRoutes.post('/', createTask);
-tasksRoutes.put('/:id', updateTask);
-tasksRoutes.delete('/:id', deleteTask);
--- server/api/utils/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (firstName: string, lastName: string) => {
-  return jwt.sign({ firstName, lastName },
-    process.env.SECRET_KEY, {
-    expiresIn: '1H'
-  });
-};
